ffbe64e5bcb44953f1968a14974a4bf400c14006
Pungraf <johny793@gmail.com>
2019-03-25 21:41:55 +0100

    Initial commit

---
+++ Assets/Script/RythmSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RythmSpawner : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public float spawnThreshold = 0.0005f;
+    public int frequency = 0;
+    public FFTWindow fftWindow;
+    
+    private float[] samples = new float[1024];
+    
+    void Update ()
+    {
+        AudioListener.GetSpectrumData(samples,0,fftWindow);
+
+
+        if(samples[frequency] > spawnThreshold)
+        {
+            Instantiate(objectPrefab,transform.position,transform.rotation);
+        }
+    }
+}
